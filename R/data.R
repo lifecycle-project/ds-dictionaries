e305dcda5b23302bdfd17e213ee92a11ad2f7fb7
erikzwart <e.zwart@umcg.nl>
2024-01-04 16:16:17 +0100

    data(lifecycle_core_2_6) documentation added (data.R)

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' Lifecycle core 2_6
+#'
+#' This dataset contains the variables and categories for the core lifecycle 2_6.
+#'
+#' @format A tibble with 8 Variable variables: \code{table}, \code{name}, \code{label}, \code{valueType}, \code{unit}, \code{repeatable}, \code{columnNamePattern}, \code{valuePattern}
+#' and Category variables: \code{table}, \code{value}, \code{missing}, \code{label}
+#' @source <https://lifecycle-project.eu/>
+"lifecycle_core_2_6"
