d29b18b096c6ba24f21db7b9fd9fada1f9ffeca7
erikzwart <e.zwart@umcg.nl>
2024-01-08 13:58:33 +0100

    add categories tests

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -14,5 +14,21 @@ test_that("expect string not empty", {
   expect_equal(sum(lifecycle_core_2_6$table==""), 0)
   expect_equal(sum(lifecycle_core_2_6$name==""), 0)
   expect_equal(sum(lifecycle_core_2_6$valueType==""), 0)
+  expect_equal(sum(lifecycle_core_2_6$label==""), 0)
+})
+
+test_that("expect zero or one", {
   expect_contains(lifecycle_core_2_6$repeatable, c(0,1))
+  expect_true(is.double(lifecycle_core_2_6$repeatable))
+})
+
+test_that("categories column names and types", {
+  t <- dplyr::bind_rows(lifecycle_core_2_6$categories)
+  if (nrow(t) > 0) {
+    expect_equal(names(t), c("table", "value", "missing", "label"))
+    expect_true(is.character(t$table))
+    expect_true(is.double(t$value))
+    expect_true(is.logical(t$missing))
+    expect_true(is.character(t$label))
+  }
 })
