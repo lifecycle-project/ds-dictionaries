0e015cba0b6aefb67a8e7f384f13bd6e9e34f9b8
erikzwart <e.zwart@umcg.nl>
2024-02-06 16:12:45 +0100

    update lifecycle_outcome_2_0 with covariate

--- data-raw/lifecycle_outcome_2_0.R
+++ data-raw/lifecycle_outcome_2_0.R
@@ -1,5 +1,6 @@
 library(readxl)
 library(tidyverse)
+dictionary_kind = "outcome"
 # Copy over the original dictionary from dictionaries/outcome/1_6 to data-raw and rename folder to lifecycle_outcome_2_0
 non_rep_v <- readxl::read_xlsx("data-raw/lifecycle_outcome_2_0/1_6_non_rep.xlsx", sheet = "Variables")
 non_rep_c <- readxl::read_xlsx("data-raw/lifecycle_outcome_2_0/1_6_non_rep.xlsx", sheet = "Categories")
@@ -14,7 +15,7 @@ yearly_rep_c <- readxl::read_xlsx("data-raw/lifecycle_outcome_2_0/1_6_yearly_rep
 
 categories <- rbind(non_rep_c,monthly_rep_c, yearly_rep_c)
 categories <- unique(categories)
-categories <- tibble::add_column(table = "outcome", categories, .before = "variable")
+categories <- tibble::add_column(table = dictionary_kind, categories, .before = "variable")
 categories <- dplyr::rename(categories, missing = isMissing)
 
 weekly_rep_v <- weekly_rep_v[!(weekly_rep_v$name %in% non_rep_v$name),]
@@ -22,13 +23,27 @@ monthly_rep_v <- monthly_rep_v[!(monthly_rep_v$name %in% non_rep_v$name),]
 yearly_rep_v <- yearly_rep_v[!(yearly_rep_v$name %in% non_rep_v$name),]
 
 non_rep_v <- tibble::add_column(repeatable = 0, non_rep_v, .before = "label")
+non_rep_v <- tibble::add_column(timeDependentCovariate = "", non_rep_v, .before = "repeatable")
 
-variables <- rbind(weekly_rep_v, monthly_rep_v, yearly_rep_v)
-variables <- tibble::add_column(repeatable = 1, variables, .before = "label")
+weekly_rep_v <- weekly_rep_v %>% dplyr::filter(name != "age_years")
 
-variables <- rbind(non_rep_v, variables)
+weekly_rep_v <- tibble::add_column(repeatable = 1, weekly_rep_v, .before = "label")
+weekly_rep_v <- tibble::add_column(timeDependentCovariate = "age_weeks", weekly_rep_v, .before = "repeatable")
+
+monthly_rep_v <- monthly_rep_v %>% dplyr::filter(name != "age_years")
+
+monthly_rep_v <- tibble::add_column(repeatable = 1, monthly_rep_v, .before = "label")
+monthly_rep_v <- tibble::add_column(timeDependentCovariate = "age_months", monthly_rep_v, .before = "repeatable")
+
+yearly_rep_v <- tibble::add_column(repeatable = 1, yearly_rep_v, .before = "label")
+yearly_rep_v <- tibble::add_column(timeDependentCovariate = "age_years", yearly_rep_v, .before = "repeatable")
+
+#variables <- rbind(weekly_rep_v, monthly_rep_v, yearly_rep_v)
+#variables <- tibble::add_column(repeatable = 1, variables, .before = "label")
+
+variables <- rbind(non_rep_v, weekly_rep_v, monthly_rep_v, yearly_rep_v)
 variables <- unique(variables)
-variables <- tibble::add_column(table = "outcome", variables, .before = "name")
+variables <- tibble::add_column(table = dictionary_kind, variables, .before = "name")
 
 variables <- tibble::add_column(columnNamePattern = "", variables, .after = "label")
 variables <- tibble::add_column(valuePattern = "", variables, .after = "columnNamePattern")
@@ -39,7 +54,7 @@ categories <- categories %>%
 
 lifecycle_outcome_2_0 <- nest_join(variables, categories, by = "name")
 
-lifecycle_outcome_2_0 <- lifecycle_outcome_2_0 %>% dplyr::filter(name!="age_years" | label!="Age of child in years")
+#lifecycle_outcome_2_0 <- lifecycle_outcome_2_0 %>% dplyr::filter(name!="age_years" | label!="Age of child in years")
 
 
 # Update NEWS.md (changelogs)
