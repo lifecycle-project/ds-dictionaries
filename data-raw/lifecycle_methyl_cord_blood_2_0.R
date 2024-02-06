351eda596f00e30ff363ae00f8d5d3efdbb9d9e7
erikzwart <e.zwart@umcg.nl>
2024-02-06 16:24:41 +0100

    update lifecycle_methyl_cord_blood_2_0 with covariate

--- data-raw/lifecycle_methyl_cord_blood_2_0.R
+++ data-raw/lifecycle_methyl_cord_blood_2_0.R
@@ -1,19 +1,22 @@
 library(readxl)
 library(tidyverse)
+dictionary_kind = "methyl_cord_blood"
 # Copy over the original dictionary from dictionaries/methyl/1_5 to data-raw and rename folder to lifecycle_methyl_cord_blood_2_0
 cord_blood_v <- readxl::read_xlsx("data-raw/lifecycle_methyl_cord_blood_2_0/1_5_corblood.xlsx", sheet = "Variables")
 cord_blood_c <- readxl::read_xlsx("data-raw/lifecycle_methyl_cord_blood_2_0/1_5_corblood.xlsx", sheet = "Categories")
 
 categories <- cord_blood_c
 categories <- unique(categories)
-categories <- tibble::add_column(table = "methyl_cord_blood", categories, .before = "variable")
+categories <- tibble::add_column(table = dictionary_kind, categories, .before = "variable")
 categories <- dplyr::rename(categories, missing = isMissing)
 
 variables <- cord_blood_v
-variables <- tibble::add_column(repeatable = 1, variables, .before = "label")
-
 variables <- unique(variables)
-variables <- tibble::add_column(table = "methyl_cord_blood", variables, .before = "name")
+variables <- variables %>% dplyr::filter(name != "row_id")
+variables <- tibble::add_column(repeatable = 1, variables, .before = "label")
+variables <- tibble::add_column(timeDependentCovariate = "age", variables, .before = "repeatable")
+
+variables <- tibble::add_column(table = dictionary_kind, variables, .before = "name")
 
 variables <- tibble::add_column(columnNamePattern = "", variables, .after = "label")
 variables <- tibble::add_column(valuePattern = "", variables, .after = "columnNamePattern")
@@ -24,9 +27,11 @@ categories <- categories %>%
 
 lifecycle_methyl_cord_blood_2_0 <- nest_join(variables, categories, by = "name")
 
-lifecycle_methyl_cord_blood_2_0 <- lifecycle_methyl_cord_blood_2_0 %>% dplyr::mutate(repeatable = ifelse(name == "row_id", 0, repeatable))
+#lifecycle_methyl_cord_blood_2_0 <- lifecycle_methyl_cord_blood_2_0 %>% dplyr::mutate(repeatable = ifelse(name == "row_id", 0, repeatable))
 lifecycle_methyl_cord_blood_2_0 <- lifecycle_methyl_cord_blood_2_0 %>% dplyr::mutate(repeatable = ifelse(name == "child_id", 0, repeatable))
-lifecycle_methyl_cord_blood_2_0 <- lifecycle_methyl_cord_blood_2_0 %>% dplyr::filter(name!="age" | label!="Chronological age at the time of DNA methylation measurement in methylclock")
+lifecycle_methyl_cord_blood_2_0 <- lifecycle_methyl_cord_blood_2_0 %>% dplyr::mutate(timeDependentCovariate = ifelse(name == "child_id", "", timeDependentCovariate))
+
+#lifecycle_methyl_cord_blood_2_0 <- lifecycle_methyl_cord_blood_2_0 %>% dplyr::filter(name!="age" | label!="Chronological age at the time of DNA methylation measurement in methylclock")
 
 # Update NEWS.md (changelogs)
 # - Compare the old and new data variables and categories
