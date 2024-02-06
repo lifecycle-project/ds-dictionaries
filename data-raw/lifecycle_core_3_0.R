fe740fd4bcfa59b1c404bad30d25aacb95893170
erikzwart <e.zwart@umcg.nl>
2024-02-06 16:02:00 +0100

    update lifecycle_core_3_0 with covariate

--- data-raw/lifecycle_core_3_0.R
+++ data-raw/lifecycle_core_3_0.R
@@ -1,5 +1,6 @@
 library(readxl)
 library(tidyverse)
+dictionary_kind = "core"
 # Copy over the original dictionary from dictionaries/core/2_6 to data-raw and rename folder to lifecycle_core_3_0
 non_rep_v <- readxl::read_xlsx("data-raw/lifecycle_core_3_0/2_6_non_rep.xlsx", sheet = "Variables")
 non_rep_c <- readxl::read_xlsx("data-raw/lifecycle_core_3_0/2_6_non_rep.xlsx", sheet = "Categories")
@@ -14,7 +15,7 @@ yearly_rep_c <- readxl::read_xlsx("data-raw/lifecycle_core_3_0/2_6_yearly_rep.xl
 
 categories <- rbind(non_rep_c,trimester_rep_c, yearly_rep_c)
 categories <- unique(categories)
-categories <- tibble::add_column(table = "core", categories, .before = "variable")
+categories <- tibble::add_column(table = dictionary_kind, categories, .before = "variable")
 categories <- dplyr::rename(categories, missing = isMissing)
 
 monthly_rep_v <- monthly_rep_v[!(monthly_rep_v$name %in% non_rep_v$name),]
@@ -22,13 +23,26 @@ trimester_rep_v <- trimester_rep_v[!(trimester_rep_v$name %in% non_rep_v$name),]
 yearly_rep_v <- yearly_rep_v[!(yearly_rep_v$name %in% non_rep_v$name),]
 
 non_rep_v <- tibble::add_column(repeatable = 0, non_rep_v, .before = "label")
+non_rep_v <- tibble::add_column(timeDependentCovariate = "", non_rep_v, .before = "repeatable")
 
-variables <- rbind(monthly_rep_v, trimester_rep_v, yearly_rep_v)
-variables <- tibble::add_column(repeatable = 1, variables, .before = "label")
+monthly_rep_v <- monthly_rep_v %>% dplyr::filter(name != "age_years")
 
-variables <- rbind(non_rep_v, variables)
+monthly_rep_v <- tibble::add_column(repeatable = 1, monthly_rep_v, .before = "label")
+monthly_rep_v <- tibble::add_column(timeDependentCovariate = "age_months", monthly_rep_v, .before = "repeatable")
+
+trimester_rep_v <- tibble::add_column(repeatable = 1, trimester_rep_v, .before = "label")
+trimester_rep_v <- tibble::add_column(timeDependentCovariate = "age_trimester", trimester_rep_v, .before = "repeatable")
+
+yearly_rep_v <- tibble::add_column(repeatable = 1, yearly_rep_v, .before = "label")
+yearly_rep_v <- tibble::add_column(timeDependentCovariate = "age_years", yearly_rep_v, .before = "repeatable")
+
+#variables <- rbind(monthly_rep_v, trimester_rep_v, yearly_rep_v)
+#variables <- tibble::add_column(repeatable = 1, variables, .before = "label")
+
+#variables <- rbind(non_rep_v, variables)
+variables <- rbind(non_rep_v, monthly_rep_v, trimester_rep_v, yearly_rep_v)
 variables <- unique(variables)
-variables <- tibble::add_column(table = "core", variables, .before = "name")
+variables <- tibble::add_column(table = dictionary_kind, variables, .before = "name")
 
 variables <- tibble::add_column(columnNamePattern = "", variables, .after = "label")
 variables <- tibble::add_column(valuePattern = "", variables, .after = "columnNamePattern")
